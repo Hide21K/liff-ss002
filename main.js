862eb12b6958c07ef9a78bd3f3701247002d58e5
unknown <kazuhide_nii1975@yahoo.co.jp>
2023-07-17 15:48:19 +0900

    second commit

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-import './style.css';
-import liff from '@line/liff';
+import './index.css';
+import liff from '@line/liff'
 
-liff
-  .init({
-    liffId: import.meta.env.VITE_LIFF_ID
-  })
-  .then(() => {
-    document.querySelector('#app').innerHTML = `
-    <h1>create-liff-app</h1>
-    <p>LIFF init succeeded.</p>
-    <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
-      LIFF Documentation
-    </a>
-  `;
-  })
-  .catch((error) => {
-    document.querySelector('#app').innerHTML = `
-    <h1>create-liff-app</h1>
-    <p>LIFF init failed.</p>
-    <p><code>${error}</code></p>
-    <a href="https://developers.line.biz/ja/docs/liff/" target="_blank" rel="noreferrer">
-      LIFF Documentation
-    </a>
-  `;
-  });
+
+document.addEventListener("DOMContentLoaded", function() {
+  liff
+    .init({ liffId: process.env.VITE_LIFF_ID })
+    .then(() => {
+        console.log("Success! you can do something with LIFF API here.")
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+});
+
